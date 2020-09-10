c1cafd04ee9589f2bc4f995a175022424002999e
Venodez <Venodez@users.noreply.github.com>
2020-09-10 16:02:29 +0200

    Correction de bugs

--- listes_chainees_partiel.py
+++ listes_chainees_partiel.py
@@ -1,10 +1,13 @@
 tab = [None, None]	
 	
 def ajouter(tab, element):
-    next = tab #On lui donne la valeur de la liste
-    while next[1] != None: #On incrémente i jusqu'a qu'on trouve le "None" du dernier tableau
-        next = next[1] #A chaque fois on prend le tableau suivant
-    next[1] = [element, None] #Et donc on crée le nouveau tableau avec la valeur à la place de "None"
+	next = tab #On lui donne la valeur de la liste
+	if tab[0] == None:
+		next[0] = element
+	else:
+		while next[1] != None: #On incrémente i jusqu'a qu'on trouve le "None" du dernier tableau
+			next = next[1] #A chaque fois on prend le tableau suivant
+		next[1] = [element, None] #Et donc on crée le nouveau tableau avec la valeur à la place de "None"
 
 def inserer(tab, index, element):
     next = tab #On lui donne la valeur de la liste
@@ -21,7 +24,7 @@ def acceder(tab, index):
 def longueur(tab):
     next = tab #On lui donne la valeur de la liste
     i = 0 #Correspondra à la taille
-    tab[0] == None: #Si la liste est vide
+    if tab[0] == None: #Si la liste est vide
         return i
     while next[1] != None: #On incrémente i jusqu'a qu'on trouve le "None" de la dernière valeur
         i += 1
@@ -29,10 +32,21 @@ def longueur(tab):
     return i+1 #On ajoute 1 car il y a une valeur en trop
     
 def supprimer_ind(tab, index):
+	next = tab #On lui donne la valeur de la liste
+	if index == 0:
+		next[0] = next[1][0]
+		next[1] = next[1][1]
+	else:
+		for _ in range(index-1): #On continue jusqu'à l'index-1 donné pour avoir le tableau juste avant celui de l'index
+			next = next[1] #A chaque fois on prend le tableau suivant
+		next[1] = next[1][1] #Et donc remet à l'ancienne valeur la suite de la liste
+
+def supprimer_val(tab, valeur):
     next = tab #On lui donne la valeur de la liste
-    for _ in range(index-1): #On continue jusqu'à l'index-1 donné pour avoir le tableau juste avant celui de l'index
-         next = next[1] #A chaque fois on prend le tableau suivant
-    next[1] =  next[1][1] : #Et donc remet à l'ancienne valeur la suite de la liste
+    while next[0] != valeur:
+        old = next
+        next = next[1]
+    old[1] = next[1]
 
 def modifier(tab, index, value):
     next = tab #On lui donne la valeur de la liste
@@ -43,3 +57,17 @@ def modifier(tab, index, value):
 def vider(tab):
     tab = [None, None] #Je remet les valeurs initiales
 
+ajouter(tab,5)
+ajouter(tab,2)
+print(tab)
+inserer(tab, 1, 3)
+print(tab)
+acceder(tab,1)
+print(longueur(tab))
+supprimer_ind(tab,1)
+print(tab)
+supprimer_val(tab,2)
+print(tab)
+modifier(tab, 0, 2)
+vider(tab)
+print(tab)
